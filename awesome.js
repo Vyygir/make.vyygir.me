d4049ede7a303164b4c546730d7823e4f404ea00
Vyygir <open.source@vyygir.me>
2023-05-29 22:03:58 +0100

    Fix scrolling on mobile devices

--- awesome.js
+++ awesome.js
@@ -5,6 +5,7 @@ document.addEventListener(
 		const $sections = document.querySelectorAll('.page');
 
 		const SCROLLER_ANIMATE_MS = 650;
+		const MOBILE_TOUCH_THRESHOLD = 5;
 
 		if (!$scroller || !$sections) {
 			return;
@@ -13,39 +14,61 @@ document.addEventListener(
 		let current = 0;
 		let scrolling = false;
 
-		document.addEventListener(
-			'wheel',
-			(event) => {
-				if (scrolling) {
+		function bringSectionIntoViewport(delta) {
+			if (scrolling) {
+				return;
+			}
+
+			console.log(delta);
+
+			if (delta > 0) {
+				if (current + 1 >= $sections.length) {
 					return;
 				}
 
-				let delta = event.deltaY;
-
-				if (delta > 0) {
-					if (current + 1 >= $sections.length) {
-						return;
-					}
-
-					current += 1;
-				} else if (delta < 0) {
-					if (current - 1 < 0) {
-						return;
-					}
-
-					current -= 1;
+				current += 1;
+			} else if (delta < 0) {
+				if (current - 1 < 0) {
+					return;
 				}
 
-				scrolling = true;
-
-				let $section = $sections.item(current);
-
-				$scroller.style.transform = `translateY(${-$section.clientHeight * current}px)`;
-
-				setTimeout(() => {
-					scrolling = false;
-				}, SCROLLER_ANIMATE_MS);
+				current -= 1;
 			}
-		)
+
+			scrolling = true;
+
+			let $section = $sections.item(current);
+
+			$scroller.style.transform = `translateY(${-$section.clientHeight * current}px)`;
+
+			setTimeout(() => {
+				scrolling = false;
+			}, SCROLLER_ANIMATE_MS);
+		}
+
+		let mobileTouchStart = 0;
+
+		document.addEventListener(
+			'touchstart',
+			(event) => {
+				mobileTouchStart = event.changedTouches[0].clientY;
+			}
+		);
+
+		document.addEventListener(
+			'touchend',
+			(event) => {
+				bringSectionIntoViewport(
+					mobileTouchStart > event.changedTouches[0].clientY + MOBILE_TOUCH_THRESHOLD ? 1 : -1
+				);
+			}
+		);
+
+		document.addEventListener(
+			'wheel',
+			(event) => {
+				bringSectionIntoViewport(event.deltaY);
+			}
+		);
 	}
 );
